0fcbd75eca10aba493f855f787cd9c8998bddd77
Ma Lian <malhch@gmail.com>
2016-08-15 01:25:25 +0800

    2016-08-15 01:24 功能: HTML 预处理

--- app.js
+++ app.js
@@ -75,8 +75,95 @@ document.addEventListener("DOMContentLoaded", function(event) {
   window.preprocess = function () {
     rawHtml     = $('#raw-html').val();
     $('#raw-html').autoHeight();
+    /// 至少可以去除 body 以外（含 body）标签
     var domTree = new DOMParser().parseFromString(rawHtml, 'text/html');
-    cleanHtml   = domTree.body.innerHTML
+    var allowedTags   = /^(body|script|div|p|h[1-6]|a|img|blockquote|ul|ol|li|pre|code|hr|em|strong|br|span|b|i)$/i
+    var passbyTags    = /^(div|span)$/i
+    var newLineTags   = /^()$/i
+    var blockTags     = /^(p|h[1-6]|script|blockquote|ul|li|ol|pre|hr)$/i
+    var selfCloseTags = /^(img|br|hr)$/i
+    var contentTags   = /^(p|h[1-6]|a|span|em|strong|pre|li|code|b|i)$/i
+
+    function parseNode(node) {
+      var row, rows = [], tag, childRows;
+      var str, re;
+      if (node.tagName.match(contentTags)) {
+        rows = node.innerHTML.split(/<([a-z]+)[^>]*>[\s\S]*?<\/\1>|<(hr|br|img)(?:[^>]*)(?: | \/)?>/);
+      }
+      for (var i = 0, child; child = node.children[i++];) {
+        tag = child.tagName.toLowerCase();
+        if (!tag.match(allowedTags)) continue;
+
+        row = [];
+        if (!tag.match(passbyTags)) {
+          row.push('<' + tag);
+          /// 2016-08-14 20:22 Sunday
+          /// - 很奇怪，child.src, child.href 等直接取值为空，用 getAttribute 就行得通
+          child.id && row.push(' id="' + child.id + '"');
+          child.className && row.push(' class="' + child.className + '"');
+          child.getAttribute('alt') && row.push(' alt="' + child.getAttribute('alt') + '"');
+          child.getAttribute('src') && row.push(' src="' + child.getAttribute('src') + '"');
+          if (str = child.getAttribute('href')) {
+            row.push(' href="' + str.replace(/&/g, '&amp;') + '"');
+          }
+          child.title && row.push(' title="' + child.title + '"');
+          row.push('>');
+        }
+
+        if (tag.match(contentTags)) {
+          if (child.children.length) {
+            row.push(parseNode(child).join(''));
+          } else {
+            row.push(child.innerHTML);
+          }
+          tag.match(newLineTags) && row.push('\n');
+        } else if (!tag.match(selfCloseTags)) {
+          childRows = parseNode(child)
+          if (childRows.length) {
+            if (tag == 'blockquote' || tag == 'ul' || tag == 'ol') {
+              row.push('\n');
+              row.push(childRows.join('\n\n'));
+              row.push('\n');
+            } else {
+              rows = rows.concat(childRows);
+            }
+          }
+        }
+
+        if (!tag.match(passbyTags) && !tag.match(selfCloseTags)) {
+          row.push('</' + tag + '>');
+        }
+
+        if (node.tagName.match(contentTags)) {
+          rows.splice(i * 3 - 2, 2, row.join(''), '');
+        } else {
+          row.length && rows.push(row.join(''));
+        }
+      }
+
+      return rows;
+    }
+
+    cleanHtml = parseNode(domTree.body).join('\n\n');
+    // $('#html-vs-upagain').html('').append(
+    //   diff(rawHtml, cleanHtml, "Cleaned HTML", "Up-again HTML")
+    // );
+    // return console.log(cleanHtml.join('\n\n'));
+    /// 去行首空白
+    // cleanHtml = cleanHtml.replace(/(^|\n)[^\S\n]+/g, '\n');
+    // /// 去注释
+    // cleanHtml = cleanHtml.replace(/<!--[\s\S]*?-->/g, '\n');
+    // /// 2016-08-14 10:05 Sunday
+    // /// - JavaScript 正则还不能处理嵌套，所以用分别删除 <div...> 和 </div> 的办法
+    // cleanHtml = cleanHtml.replace(/<(div|textarea)\s?[^>]*>|<\/(div|textarea)>/g, '');
+    // /// 去空 p
+    // cleanHtml = cleanHtml.replace(/<p\s?[^>]*>\s*<\/p>/g, '');
+    // /// 去多行空白
+    // cleanHtml = cleanHtml.replace(/\n\n+/g, '\n\n');
+    // /// 去头尾空白
+    // cleanHtml = cleanHtml.replace(/^\s+/, '').replace(/\s+$/, '');
+    // cleanHtml = cleanHtml.replace(/<h1>\s+/g, '<h1>');
+    // cleanHtml = cleanHtml.replace(/\s+<\/h1>/g, '</h1>');
     $('#clean-html').html('').append(diffWords(rawHtml, cleanHtml));
 
     $('#converted-markdown').val('').autoHeight();
